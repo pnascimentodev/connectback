b0e35c827d193762c29c6bab1ece06b2156833d6
Priscila <pnmelo2808@gmail.com>
2024-12-06 16:01:29 -0300

    Corretion

--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { SaleService } from './sales.service';
 import { SaleController } from './sales.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [SaleService],
   controllers: [SaleController],
 })
